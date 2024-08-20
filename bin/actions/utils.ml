9b4ec20b0b7c545cfbe90a8218b0fc8742287755
Etienne Marais <dev@maiste.fr>
2024-08-20 18:02:13 +0200

    feat: use core yocaml function instead of custom impl

--- bin/actions/utils.ml
+++ bin/actions/utils.ml
@@ -1,18 +1,6 @@
-
 (** Generic *)
 let process_markdown ?(only = `Both) source processor =
   Yocaml.Action.batch ~only ~where:Rule.is_md source processor
 ;;
 
-(* FIXME: Ugly function? Need refactoring *)
-let process_actions (actions : Yocaml.Action.t list) (cache : Yocaml.Cache.t) : Yocaml.Cache.t Yocaml.Eff.t =
-  let open Yocaml.Eff in
-  let rec aux actions (cache : Yocaml.Cache.t Yocaml.Eff.t) =
-    match actions with
-    | [] -> cache
-    | action :: actions ->
-      let cache = cache >>= action in
-      aux actions cache
-  in
-  aux actions (return cache)
-;;
+let process_actions actions = Yocaml.Action.batch_list actions Fun.id
