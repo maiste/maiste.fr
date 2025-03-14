eec9b60fc2b9055ea8c6dbb6b4e8fa51831bf1f7
Etienne Marais <dev@maiste.fr>
2025-03-14 14:59:01 +0100

    chore: move to bin/lib

--- bin/maiste.ml
+++ bin/maiste.ml
@@ -1,4 +1,4 @@
-open Yocaml
+module Resolver = Lib.Resolver
 
 let build ~source ~target log_level =
   let module R =
@@ -7,24 +7,24 @@ let build ~source ~target log_level =
       let target = target
     end)
   in
-  let process_all = Actions.All.process (module R) in
+  let process_all = Lib.Actions.All.process (module R) in
   Yocaml_eio.run ~level:log_level process_all
 ;;
 
 let serve ~source ~target log_level port =
   let module R =
-    Resolver.Make (struct
+    Lib.Resolver.Make (struct
       let source = source
       let target = target
     end)
   in
-  let process_all = Actions.All.process (module R) in
+  let process_all = Lib.Actions.All.process (module R) in
   Yocaml_eio.serve ~level:log_level ~target:R.Target.root ~port process_all
 ;;
 
 let () =
-  let source = Path.rel [] in
-  let target = Path.rel [ "target" ] in
+  let source = Lib.Path.rel [] in
+  let target = Lib.Path.rel [ "target" ] in
   match Array.to_list Sys.argv with
   | [ _; "serve" ] -> serve ~source ~target `Debug 8085
   | _ -> build ~source ~target `Debug
---
+++ lib/lib.ml
@@ -0,0 +1,4 @@
+module S = S
+module Actions = Actions
+module Resolver = Resolver
+module Path = Yocaml.Path
